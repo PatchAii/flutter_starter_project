72b97dc441615dd1d377fd51cca50dd4ee4e21d7
dbbd59 <davide.bolzoni@patchai.io>
2021-09-06 16:00:04 +0200

    fix: updates path name.

--- test/utils/fixture_reader.dart
+++ test/utils/fixture_reader.dart
@@ -2,4 +2,4 @@ import 'dart:convert';
 import 'dart:io';
 
 Map<String, dynamic> fixture(String name) =>
-    jsonDecode(File('test/fixtures/json/$name').readAsStringSync());
+    jsonDecode(File('test/utils/json/$name').readAsStringSync());
