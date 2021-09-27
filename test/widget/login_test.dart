37bd461b3c4acc25a0c3fd476c19c5036a7faed9
GangemiLorenzo <lorenzo.gangemi.developer@gmail.com>
2021-09-27 17:44:10 +0200

    feat: adds login goldes, adds onboarding goldens

---
+++ test/widget/login_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_starter_project/core/core.dart';
+import 'package:flutter_starter_project/feature/feature.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+
+import '../utils/golden_device_builder.dart';
+
+Future<void> main() async {
+  EasyLocalization.logger.enableBuildModes = [];
+  await loadAppFonts();
+  group('LoginView', () {
+    testGoldens('Login Page', (tester) async {
+      await mockNetworkImagesFor(() async {
+        final builder = getDefaultBuilder(
+          const LoginPage(),
+        );
+        await tester.pumpDeviceBuilder(builder);
+        await screenMatchesGolden(tester, 'login_page');
+      });
+    });
+  });
+}
