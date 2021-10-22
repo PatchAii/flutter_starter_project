fd13f1f8d7388dacf11f953abcdd7b0249345267
GangemiLorenzo <lorenzo.gangemi.developer@gmail.com>
2021-10-22 11:20:40 +0200

    test: adds posts repo test

--- lib/feature/posts/repo/posts_repo.dart
+++ lib/feature/posts/repo/posts_repo.dart
@@ -1,25 +1,19 @@
 import 'dart:convert';
 
-import 'package:flutter_starter_project/core/core.dart';
 import 'package:flutter_starter_project/model/post/post.dart';
 import 'package:flutter_starter_project/utils/utils.dart';
 import 'package:injectable/injectable.dart';
 
 @injectable
 class PostsRepo {
-  Future<Post> fetchPost(int postId) async {
-    final res = await getIt<RestClient>().get(
-      api: 'posts/$postId',
-      endpoint: 'https://jsonplaceholder.typicode.com/',
-    );
+  PostsRepo({
+    required this.restClient,
+  });
 
-    final post = Post.fromJson(jsonDecode(res.body));
-
-    return post;
-  }
+  final RestClient restClient;
 
   Future<List<Post>> fetchPosts() async {
-    final res = await getIt<RestClient>().get(
+    final res = await restClient.get(
       api: 'posts',
       endpoint: 'https://jsonplaceholder.typicode.com/',
     );
@@ -36,7 +30,7 @@ class PostsRepo {
   }
 
   Future<List<Post>> fetchPostsByUserId(String userId) async {
-    final res = await getIt<RestClient>().get(
+    final res = await restClient.get(
       api: 'posts/?userId=$userId',
       endpoint: 'https://jsonplaceholder.typicode.com/',
     );
